17c27ec914a760aa8f319c0719feb8f4c9045182
akhil1427 <akhilkonda61@gmail.com>
2023-11-07 23:06:14 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def akhil (request):
+    return HttpResponse ('<h2><marquee> Akhil:sai love silk</h2></marquee>')
